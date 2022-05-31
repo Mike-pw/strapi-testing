0ee2e8e11944396dceef1d0ed347d104644fc95d
Mike Melgren <mikemelgren@gmail.com>
2022-05-31 08:38:45 -0500

    remove test keys

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("MY_HEROKU_URL"),
-    proxy: true,
-    app: {
-      keys: env.array("APP_KEYS", ["jP8pb1lYsAhnmURarewxhA==", "34xnLMYHY5jiU7ONTstTqQ=="]),
-    },
-  });
